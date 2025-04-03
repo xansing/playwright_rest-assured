1ae9af5b563d4ab9a8841cbbbbaa644052b18f9e
Mike Carrillo <gmikecarrillo@gmail.com>
2025-04-03 00:14:12 -0300

    milestone: Refactorizado con Page Object - UserApi y tests funcionando

---
+++ API_tests/src/test/java/com/mike/gorest/APIs/UserApi.java
@@ -0,0 +1,55 @@
+package com.mike.gorest.APIs;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+
+
+
+
+public class UserApi {
+    private static final String BASE_URL = "https://gorest.co.in/public/v2";
+    
+    //Tenes que setear como variable entorno local el nombre "GOREST_TOKEN" con tu token de Go rest para que lo pueda consumir
+    
+    private String getToken() {
+        String token = System.getenv("GOREST_TOKEN");
+        if (token == null) {
+            throw new IllegalStateException("GOREST_TOKEN is not set.");
+        }
+        return token;
+    }
+
+
+    
+    
+
+    public Response getUsers() {
+        RestAssured.baseURI = BASE_URL;
+        System.out.println("TOKEN ACTUAL: " + getToken());
+
+        return given().header("Authorization", getToken()).when().get("/users");
+    }
+
+
+    public Response createUSer(String name, String email, String gender, String status){
+        RestAssured.baseURI = BASE_URL;
+        System.out.println("TOKEN ACTUAL: " + getToken());
+        String requestBody = String.format("""
+        {
+            "name": "%s", "email": "%s", "gender": "%s", "status": "%s"
+        }
+        """, name, email, gender, status);
+
+        return given().header("Authorization", getToken()).contentType("application/json").body(requestBody).when().post("/users");
+    }
+
+    public Response getUserById(int userId){
+        System.out.println("TOKEN ACTUAL: " + getToken());
+        RestAssured.baseURI = BASE_URL;
+        return given().header("Authorization",getToken()).when().get("/users/" + userId);
+    }
+    
+
+}
---
+++ API_tests/src/test/java/com/mike/gorest/Tests/UserApiTest.java
@@ -0,0 +1,52 @@
+package com.mike.gorest.Tests;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+import com.mike.gorest.APIs.UserApi;
+
+import io.restassured.response.Response;
+
+public class UserApiTest {
+    UserApi userApi = new UserApi();
+
+    //Objetivo del test: recibir un listado de los users validando el codigo de respuesta y que devuelva al menos uno
+    @Test
+    public void getUsers_shouldReturnListOfUsers(){
+        userApi.getUsers().then().statusCode(200).body("size()", greaterThan(0));
+    }
+
+
+    //Objetivo del test: Crear un usuario mike + numero aleatorio@test.com y validar codigo de respuesta, un id, email el mismo que se mando y nombre igual al que se mando
+    @Test
+    public void createUsers_shouldCreateAndReturnUserData(){
+    
+    //use System.currentTimeMillis() en vez de UUID.randomUUID() para asegurar que sea imposible colision de datos existentes
+        String email = "mike" + UUID.randomUUID() + "@test.com";
+
+        userApi.createUSer("Mike Test", email, "male", "active").then().statusCode(201)
+        .body("id", notNullValue()).body("email", equalTo(email)).body("name", equalTo("Mike Test"));
+    }
+
+    //Objetivo del test: Crear un usuario y despues con su ID consultar ese ID y validar el codigo de respuesta, ID, email y nombre
+    @Test
+    public void getUserById_shouldReturnUserDetails(){
+        //use System.currentTimeMillis() en vez de UUID.randomUUID() para asegurar que sea imposible colision de datos existentes
+        String email = "mike" + UUID.randomUUID() + "@test.com";
+
+        //Crear el usuario y retener el ID
+        Response creaResponse = userApi.createUSer("Mike Creation", email, "male", "active");
+        creaResponse.then().statusCode(201);
+        int userId= creaResponse.jsonPath().getInt("id");
+        
+        //utilizando el ID valido que devuelve los valores que espero
+        userApi.getUserById(userId).then().statusCode(200).body("id", equalTo(userId))
+        .body("email", equalTo(email)).body("name", equalTo("Mike Creation"));
+    }
+}
+
